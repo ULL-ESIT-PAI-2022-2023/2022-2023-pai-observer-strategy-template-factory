b7d1191c19c6292533e280e33d9f0fafa65a7104
Thomas Bradley <alu0101408248@ull.edu.es>
2023-03-14 18:54:10 +0000

    template-method.ts propperly commented

--- src/template-method/template-method.ts
+++ src/template-method/template-method.ts
@@ -10,14 +10,11 @@
  */
 
 /**
- * The Abstract Class defines a template method that contains a skeleton of some
- * algorithm, composed of calls to (usually) abstract primitive operations.
+ * @class The Abstract Class defines a template method that contains a skeleton of some
+ * algorithm, composed (usually) of calls to abstract primitive operations.
  */
 abstract class GameAI {
-  /**
-   * The template method defines the skeleton of an algorithm, subclasses must 
-   * leave this intact
-   */
+  /** @desc The template method, subclasses must leave this intact */
   public templateMethod(): void {
     // Implemented in the Abstract class
     this.heal();
@@ -34,29 +31,36 @@ abstract class GameAI {
   }
 
   /** These operations already have implementations */
+  /** @desc Prints dialogue for when the AI is healing */
   protected heal(): void {
     console.log('I need to heal');
   }
 
+  /** @desc Prints dialogue for when the AI is runnign away */
   protected runAway(): void {
     console.log('I am running away');
   }
 
+  /** @desc Prints dialogue for when the AI is picking up an item */
   protected pickUpItem(): void {
     console.log('I am going to pick up an item');
   }
 
   /** These operations have to be implemented in subclasses */
+  /** @desc Prints dialogue for when the AI is trying to raise an alarm*/
   protected abstract raiseAlarm(): void;
 
+  /** @desc Prints dialogue for when the AI is talking to the player */
   protected abstract talkToPlayer(): void;
 
   /**
-   * These are hooks, they can be overriden (if not they already have an empty
+   * These are hooks, they can be overriden (if not, they already have an empty
    * implementation in the abstract class)
    */
+  /** @desc Prints dialogue for when the AI is attacking the player */
   protected attackPlayer(): void { }
 
+  /** @desc Prints dialogue for when the AI is asking for help */
   protected askPlayerForHelp(): void { }
 }
 
@@ -64,47 +68,56 @@ abstract class GameAI {
 * Concrete classes have to implement all abstract operations of the base class.
 * They can also override hooks with a default implementation.
 */
+/** @class Represents AI that is friendly towards the player */
 class FriendlyAI extends GameAI {
+  /** @desc Prints dialogue for when the AI is trying to raise an alarm, subclass is forced to implement */
   protected raiseAlarm(): void {
     console.log('Sound the alarm, call the player');
   }
 
+  /** @desc Prints dialogue for when the AI is talking to the player, subclass is forced to implement */
   protected talkToPlayer(): void {
     console.log('Thank goodness you were there to save us');
   }
+
+  /** @desc Prints dialogue for when the AI is asking for help, subclass choses to implement this */
   protected askPlayerForHelp(): void {
     console.log('I left a prized family heirloom at my old house, could you help me retrieve it?');
   }
 }
 
+/** @class Represents AI that will be hostile to the player */
 class EnemyAI extends GameAI {
+  /** @desc Prints dialogue for when the AI is trying to raise an alarm, subclass is forced to implement */
   protected raiseAlarm(): void {
     console.log('Sound the alarm, the player is attacking!');
   }
 
+  /** @desc Prints dialogue for when the AI is talking to the player, subclass is forced to implement */
   protected talkToPlayer(): void {
     console.log('You will never defeat me!');
   }
+
+  /** @desc Prints dialogue for when the AI is attacking the player, subclass choses to implement this */
   protected attackPlayer(): void {
     console.log('There he is, attack!');
   }
 }
 
 /**
-* The client code calls the template method to execute the algorithm. Client
-* code does not have to know the concrete class of an object it works with, as
-* long as it works with objects through the interface of their base class.
-*/
-function clientCode(nonPlayableCharacter: GameAI) {
+ * @desc Calls the template method to execute the algorithm through the interface of their base class.
+ * @param {GameAI} nonPlayableCharacter Concrete class we're going to call the template method for
+ */
+function clientCode(nonPlayableCharacter: GameAI): void {
   nonPlayableCharacter.templateMethod();
 }
 
 function main() {
-  console.log('Possible actions for a friendly AI:');
+  console.log('Possible dialogue for a friendly AI:');
   clientCode(new FriendlyAI());
   console.log('');
 
-  console.log('Possible actions for an enemy AI:');
+  console.log('Possible dialogue for an enemy AI:');
   clientCode(new EnemyAI());
 }
 
