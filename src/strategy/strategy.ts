81201052f6daa4a38725292f8272d24a5586abf7
Daniel Mendez <alu0101391793@ull.edu.es>
2023-03-16 10:56:12 +0000

    Strategy example coded and commented

--- src/strategy/strategy.ts
+++ src/strategy/strategy.ts
@@ -1,82 +1,60 @@
+
+/** @desc Defines what each strategy of the calculator should have */
+interface CalculatorStrategy {
+  calculate(firstNumber: number, secondNumber: number): number;
+}
+
+/** @desc Strategy that adds two numbers */
+class AdditionStrategy implements CalculatorStrategy {
+  public calculate(firstNumber: number, secondNumber: number): number {
+    return firstNumber + secondNumber;
+  }
+}
+
+/** @desc Strategy that subtracts two numbers */
+class SubtractionStrategy implements CalculatorStrategy {
+  public calculate(firstNumber: number, secondNumber: number): number {
+    return firstNumber - secondNumber;
+  }
+}
+
+/** @desc Strategy that multiplies two numbers */
+class MultiplicationStrategy implements CalculatorStrategy {
+  public calculate(firstNumber: number, secondNumber: number): number {
+    return firstNumber * secondNumber;
+  }
+}
+
+/** @desc Context that will use the strategies */
+class CalculatorContext {
+  constructor(private strategy: CalculatorStrategy) {
+    this.strategy = strategy;
+  }
+
+  /** @desc Method that allows to change the strategy used during execution */
+  public setStrategy(strategy: CalculatorStrategy): void {
+    this.strategy = strategy;
+  }
+
+  /** @desc Method that executes the strategy */
+  public executeStrategy(firstNumber: number, secondNumber: number): number {
+    console.log('Executing strategy with numbers: ', firstNumber, secondNumber);
+    return this.strategy.calculate(firstNumber, secondNumber);
+  }
+}
+
 /**
- * The Context defines the interface of interest to clients.
+ * Client code can work with any strategy
  */
-class Context {
-  /**
-   * @type {Strategy} The Context maintains a reference to one of the Strategy
-   * objects. The Context does not know the concrete class of a strategy. It
-   * should work with all strategies via the Strategy interface.
-   */
-  private strategy: Strategy;
+console.log('Welcome to the calculator');
+console.log('===========================');
+const firstNumber = 10;
+const secondNumber = 5;
 
-  /**
-   * Usually, the Context accepts a strategy through the constructor, but also
-   * provides a setter to change it at runtime.
-   */
-  constructor(strategy: Strategy) {
-      this.strategy = strategy;
-  }
-
-  /**
-   * Usually, the Context allows replacing a Strategy object at runtime.
-   */
-  public setStrategy(strategy: Strategy) {
-      this.strategy = strategy;
-  }
-
-  /**
-   * The Context delegates some work to the Strategy object instead of
-   * implementing multiple versions of the algorithm on its own.
-   */
-  public doSomeBusinessLogic(): void {
-      // ...
-
-      console.log('Context: Sorting data using the strategy (not sure how it\'ll do it)');
-      const result = this.strategy.doAlgorithm(['a', 'b', 'c', 'd', 'e']);
-      console.log(result.join(','));
-
-      // ...
-  }
-}
-
-/**
-* The Strategy interface declares operations common to all supported versions
-* of some algorithm.
-*
-* The Context uses this interface to call the algorithm defined by Concrete
-* Strategies.
-*/
-interface Strategy {
-  doAlgorithm(data: string[]): string[];
-}
-
-/**
-* Concrete Strategies implement the algorithm while following the base Strategy
-* interface. The interface makes them interchangeable in the Context.
-*/
-class ConcreteStrategyA implements Strategy {
-  public doAlgorithm(data: string[]): string[] {
-      return data.sort();
-  }
-}
-
-class ConcreteStrategyB implements Strategy {
-  public doAlgorithm(data: string[]): string[] {
-      return data.reverse();
-  }
-}
-
-/**
-* The client code picks a concrete strategy and passes it to the context. The
-* client should be aware of the differences between strategies in order to make
-* the right choice.
-*/
-const context = new Context(new ConcreteStrategyA());
-console.log('Client: Strategy is set to normal sorting.');
-context.doSomeBusinessLogic();
-
+console.log('Client A: I want to add two numbers');
+const calculator = new CalculatorContext(new AdditionStrategy());
+console.log('Result: ', calculator.executeStrategy(firstNumber, secondNumber));
 console.log('');
-
-console.log('Client: Strategy is set to reverse sorting.');
-context.setStrategy(new ConcreteStrategyB());
-context.doSomeBusinessLogic();
+console.log('Client B: I want to multiply two numbers');
+calculator.setStrategy(new MultiplicationStrategy());
+console.log('Result: ', calculator.executeStrategy(firstNumber, secondNumber));
